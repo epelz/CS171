1a1e51be1c9863e85759556dd4bb44ded199a3b7
Eric Pelz <eric+github@ericpelz.com>
2013-12-04 13:39:32 -0800

    [hw7a] Use quaternions to interpolate rotations
    Quaternions are very useful for representing rotations, so they are used
    here when interpolating the rotations. Rotation data is converted to
    quaternion form, interpolated, and then converted back.

--- hw7a/catmullRomSpline.py
+++ hw7a/catmullRomSpline.py
@@ -1,22 +1,49 @@
 # Eric Pelz
 # CS 171: Introduction to Computer Graphics
 from matrix import MatrixExtended
+import math
 
 global tension
 tension = 0.5
+
+def toQuaternion(inTuple):
+  """Convert from input tuple [x, y, z, angle] to quaternion."""
+  x, y, z, angle = inTuple
+  angleR = math.radians(angle)
+  cs = math.cos(angleR / 2.0)
+  sn = math.sin(angleR / 2.0)
+  return [cs, x * sn, y * sn, z * sn]
+def vec4Unit(vec):
+  """Take an input 4-vector and return its unit vector."""
+  def vecMagnitude(v):
+    return sum(map(lambda x: x ** 2, v)) ** 0.5
+  assert len(vec) == 4 # assert 4-vector
+  mag = vecMagnitude(vec)
+  unitVec = map(lambda x: x / mag, vec)
+  assert 1.0 - vecMagnitude(unitVec) < 1e5 # assert unit vector
+  return unitVec
+def fromQuaternion(inVec4):
+  return inVec4[1:] + [math.degrees(2 * math.acos(inVec4[0]))]
 
 class CRSpline():
   def __init__(self, numFrames, keyFrames):
     self.numFrames = numFrames
     self.keyFrames = keyFrames
 
-    # interpolate all transformation data
+    ## interpolate all transformation data ##
+    # interpolate translation data
     self.translationData = self.interpolate(
         map(lambda kf: {'data': kf.translation.data, 'num': kf.num}, keyFrames)
     )
-    self.rotationData = self.interpolate(
-        map(lambda kf: {'data': kf.rotation.data, 'num': kf.num}, keyFrames)
+
+    # interpolate rotation data
+    # note: use quaternions during interpolation, and then convert back afterwards
+    interpQuaternions = self.interpolate(
+        map(lambda kf: {'data': toQuaternion(kf.rotation.data), 'num': kf.num}, keyFrames)
     )
+    self.rotationData = map(lambda v: fromQuaternion(vec4Unit(v)), interpQuaternions)
+
+    # interpolate scaling data
     self.scaleData = self.interpolate(
         map(lambda kf: {'data': kf.scale.data, 'num': kf.num}, keyFrames)
     )
